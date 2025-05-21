b3c4c5fc6fcd0b89176db057bdc6d1862a0bde81
prashantpic <prashantpic001@gmail.com>
2025-05-21 19:19:35 +0530

    Delete  Integration-Service/src/Services/Integration directory

---  Integration-Service/src/Services/Integration/Configuration/DigitalTwinSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace IntegrationService.Configuration
-{
-    public class DigitalTwinSettings
-    {
-        public const string SectionName = "DigitalTwins";
-        public List<DigitalTwinConfig> Platforms { get; set; } = new List<DigitalTwinConfig>();
-    }
-}
---  Integration-Service/src/Services/Integration/Configuration/IoTPlatformSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace IntegrationService.Configuration
-{
-    public class IoTPlatformSettings
-    {
-        public const string SectionName = "IoTPlatforms";
-
-        public List<IoTPlatformConfig> Platforms { get; set; } = new List<IoTPlatformConfig>();
-    }
-}
